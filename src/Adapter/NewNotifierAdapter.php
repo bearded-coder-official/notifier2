ba93bd231e9b48d9736d0240e645eb7c71f9a373
Stanislav Protasevich <stanislav.protasevich@gmail.com>
2019-05-13 21:02:23 +0300

    Added new adapter

---
+++ src/Adapter/NewNotifierAdapter.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace NtSchool\Notifier\Adapter;
+
+use NtSchool\Notifier\NotifierInterface;
+
+final class NewNotifierAdapter implements NotifierInterface
+{
+    public function notify(string $message)
+    {
+        // TODO: Implement notify() method.
+    }
+}
